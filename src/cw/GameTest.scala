67be64277c326be301d64caf3124e568013a7f15
Piotr <piotrek.falba@gmail.com>
2022-10-07 15:04:59 +0100

    Update GameTest.scala
    deleted comment

--- src/cw/GameTest.scala
+++ src/cw/GameTest.scala
@@ -130,8 +130,6 @@ class GameTest {
   }
 
   
-  //The tests beyond this point are generally more difficult to pass based on the methods they are assessing, 
-  // therefore you may wish to focus on passing the tests above first.
   
   
   @Test def test_20_adarsave {
